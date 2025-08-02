864dfac112ebe0354d864127b6848149ff8cd14c
ArielGuevara <adguevara7@espe.edu.ec>
2025-08-02 17:57:28 -0500

    Creacion de la ruta  del endpoint para reporte por Materia

--- CODIGO/PintaAuto_SpringBoot/inventory-management/src/main/java/com/pintaauto/inventory/controller/ReporteController.java
+++ CODIGO/PintaAuto_SpringBoot/inventory-management/src/main/java/com/pintaauto/inventory/controller/ReporteController.java
@@ -1,6 +1,7 @@
 package com.pintaauto.inventory.controller;
 
 import com.pintaauto.inventory.entity.ReporteFechas;
+import com.pintaauto.inventory.entity.ReporteMaterias;
 import com.pintaauto.inventory.service.ReporteService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.format.annotation.DateTimeFormat;
@@ -10,17 +11,24 @@ import org.springframework.web.bind.annotation.*;
 import java.time.LocalDateTime;
 
 @RestController
-@RequestMapping("/api/reportes/fechas")
+@RequestMapping("/api/reportes")
 public class ReporteController {
 
     @Autowired
     private ReporteService reporteService;
 
-    @GetMapping
+    @GetMapping("/fechas")
     public ResponseEntity<ReporteFechas> generarReportePorFechas(
             @RequestParam("fechaInicio") @DateTimeFormat(pattern = "yyyy-MM-dd") LocalDateTime fechaInicio,
             @RequestParam("fechaFin") @DateTimeFormat(pattern = "yyyy-MM-dd") LocalDateTime fechaFin) {
         ReporteFechas reporte = reporteService.generarReportePorFechas(fechaInicio, fechaFin);
+        return ResponseEntity.ok(reporte);
+    }
+
+    @GetMapping("/materia")
+    public ResponseEntity<ReporteMaterias> generarReporteMaterias(
+            @RequestParam("nombreMateria") String nombreMateria) {
+        ReporteMaterias reporte = reporteService.generarReportePorMateriaPrima(nombreMateria);
         return ResponseEntity.ok(reporte);
     }
 }
